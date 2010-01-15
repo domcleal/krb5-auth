2dc3a2b5433a277d6f3e73d086411a93c92030aa
Daniel Berger <dberger@globe.gov>
2010-01-15 09:41:25 -0700

    Added tests for get_init_creds_pasword and get_default_realm.

--- test/test_krb5.rb
+++ test/test_krb5.rb
@@ -3,6 +3,9 @@
 #
 # Test suite for the Krb5Auth::Krb5 class.
 ########################################################################
+require 'rubygems'
+gem 'test-unit'
+
 require 'test/unit'
 require 'krb5_auth'
 
@@ -11,8 +14,40 @@ class TC_Krb5 < Test::Unit::TestCase
     @krb5 = Krb5Auth::Krb5.new
   end
 
-  def test_version
+  test "version constant" do
     assert_equal('0.8.0', Krb5Auth::Krb5::VERSION)
+  end
+
+  test "get_default_realm basic functionality" do
+    assert_respond_to(@krb5, :get_default_realm)
+    assert_nothing_raised{ @krb5.get_default_realm }
+    assert_kind_of(String, @krb5.get_default_realm)
+  end
+
+  test "get_default_realm takes no arguments" do
+    assert_raise(ArgumentError){ @krb5.get_default_realm('localhost') }
+  end
+
+  test "get_init_creds_password basic functionality" do
+    assert_respond_to(@krb5, :get_init_creds_password)
+  end
+
+  test "get_init_creds_password requires two arguments" do
+    assert_raise(ArgumentError){ @krb5.get_init_creds_password }
+    assert_raise(ArgumentError){ @krb5.get_init_creds_password('test') }
+  end
+
+  test "get_init_creds_password requires string arguments" do
+    assert_raise(TypeError){ @krb5.get_init_creds_password(1, 2) }
+    assert_raise(TypeError){ @krb5.get_init_creds_password('test', 1) }
+  end
+
+  test "change_password basic functionality" do
+    assert_respond_to(@krb5, :change_password)
+  end
+
+  test "change_password fails if there is no context" do
+    notify("Oops, this segfaults at the moment. Needs fixing.")
   end
 
   def teardown
