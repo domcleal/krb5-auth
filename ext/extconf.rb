97c73f92fbe592356048cf76681143754ac55019
Daniel Berger <dberger@globe.gov>
2010-01-20 13:00:29 -0700

    Add check for kadm5 related header and library.

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -1,8 +1,12 @@
 require 'mkmf'
 
-dir_config('krb5_auth')
+dir_config('krb5_auth', '/usr/local/include')
 
 have_header('krb5.h')
 have_library('krb5')
+
+if have_header('kadm5/admin.h')
+  have_library('kadm5clnt')
+end
 
 create_makefile('krb5_auth')
