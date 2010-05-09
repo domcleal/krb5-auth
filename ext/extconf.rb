530b45498377c4d0e068195804c76536b49e4871
Dan Berger <dberger@boromir.globe.ucar.edu>
2010-05-09 22:43:14 +0000

    Blow up build if kadm5 library not found.

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -5,7 +5,10 @@ dir_config('krb5_auth', '/usr/local')
 have_header('krb5.h')
 have_library('krb5')
 
-have_header('kadm5/admin.h')
-have_library('kadm5clnt')
+if have_header('kadm5/admin.h')
+  have_library('kadm5clnt')
+else
+  raise "kadm5clnt library not found"
+end
 
 create_makefile('krb5_auth')
