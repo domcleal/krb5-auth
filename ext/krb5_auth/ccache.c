589ac45741ed9c72fda904676c249d7d8ac23c85
Daniel Berger <dberger@globe.gov>
2010-08-13 12:44:11 -0600

    Refactored the ccache.destroy method to return true or false instead of raising an error if there is no cache. Also refactored the constructor so that the principal is optional, and a cache name is also optional.

--- ext/krb5_auth/ccache.c
+++ ext/krb5_auth/ccache.c
@@ -26,27 +26,41 @@ static VALUE rkrb5_ccache_allocate(VALUE klass){
 
 /*
  * call-seq:
- *   Krb5Auth::CredentialsCache.new('principal@realm')
+ *   Krb5Auth::CredentialsCache.new(principal = nil, cache_name = nil)
  *
- * Creates and returns a new Krb5Auth::CredentialsCache object.
+ * Creates and returns a new Krb5Auth::CredentialsCache object. If cache_name
+ * is specified, then that cache is used, which must be in "type:residual"
+ * format, where 'type' is a type known to Kerberos (typically 'FILE').
+ *
+ * If a +principal+ is specified, then it creates or refreshes the credentials
+ * cache with the primary principal set to +principal+. If the credentials
+ * cache already exists, its contents are destroyed.
+ *
+ * Note that the principal's credentials are not set via the constructor.
+ * It merely creates the cache and sets the default principal.
  */
-static VALUE rkrb5_ccache_initialize(VALUE self, VALUE v_principal){
+static VALUE rkrb5_ccache_initialize(int argc, VALUE* argv, VALUE self){
   RUBY_KRB5_CCACHE* ptr;
   krb5_error_code kerror;
+  VALUE v_principal, v_name;
 
   Data_Get_Struct(self, RUBY_KRB5_CCACHE, ptr);
 
-  Check_Type(v_principal, T_STRING);
+  rb_scan_args(argc, argv, "02", &v_principal, &v_name);
 
   // Convert the principal name to a principal object
-  kerror = krb5_parse_name(
-    ptr->ctx,
-    StringValuePtr(v_principal),
-    &ptr->principal
-  );
+  if(RTEST(v_principal)){
+    Check_Type(v_principal, T_STRING);
 
-  if(kerror)
-    rb_raise(cKrb5Exception, "krb5_parse_name: %s", error_message(kerror));
+    kerror = krb5_parse_name(
+      ptr->ctx,
+      StringValuePtr(v_principal),
+      &ptr->principal
+    );
+
+    if(kerror)
+      rb_raise(cKrb5Exception, "krb5_parse_name: %s", error_message(kerror));
+  }
 
   // Initialize the context
   kerror = krb5_init_context(&ptr->ctx);
@@ -54,17 +68,28 @@ static VALUE rkrb5_ccache_initialize(VALUE self, VALUE v_principal){
   if(kerror)
     rb_raise(cKrb5Exception, "krb5_init_context: %s", error_message(kerror));
 
-  // Set the credentials cache using the default cache
-  kerror = krb5_cc_default(ptr->ctx, &ptr->ccache);
+  // Set the credentials cache using the default cache if no name is provided
+  if(NIL_P(v_name)){
+    kerror = krb5_cc_default(ptr->ctx, &ptr->ccache);
 
-  if(kerror)
-    rb_raise(cKrb5Exception, "krb5_cc_default: %s", error_message(kerror));
+    if(kerror)
+      rb_raise(cKrb5Exception, "krb5_cc_default: %s", error_message(kerror));
+  }
+  else{
+    Check_Type(v_name, T_STRING);
+    kerror = krb5_cc_resolve(ptr->ctx, StringValuePtr(v_name), &ptr->ccache);
 
-  // Initialize the credentials cache
-  kerror = krb5_cc_initialize(ptr->ctx, ptr->ccache, ptr->principal);
+    if(kerror)
+      rb_raise(cKrb5Exception, "krb5_cc_resolve: %s", error_message(kerror));
+  }
 
-  if(kerror)
-    rb_raise(cKrb5Exception, "krb5_cc_initialize: %s", error_message(kerror));
+  // Initialize the credentials cache if a principal was provided
+  if(RTEST(v_principal)){
+    kerror = krb5_cc_initialize(ptr->ctx, ptr->ccache, ptr->principal);
+
+    if(kerror)
+      rb_raise(cKrb5Exception, "krb5_cc_initialize: %s", error_message(kerror));
+  }
   
   return self;
 }
@@ -107,6 +132,9 @@ static VALUE rkrb5_ccache_close(VALUE self){
  *   ccache.default_name
  *
  * Returns the name of the default credentials cache.
+ *
+ * This is typically a file under /tmp with a name like 'krb5cc_xxxx',
+ * where 'xxxx' is the uid of the current process owner.
  */
 static VALUE rkrb5_ccache_default_name(VALUE self){
   RUBY_KRB5_CCACHE* ptr;
@@ -125,10 +153,14 @@ static VALUE rkrb5_ccache_default_name(VALUE self){
  *
  * Destroy the credentials cache of the current principal. This also closes
  * the object and it cannot be reused.
+ *
+ * If the cache was destroyed then true is returned. If there is no cache
+ * then false is returned.
  */
 static VALUE rkrb5_ccache_destroy(VALUE self){
   RUBY_KRB5_CCACHE* ptr;
   krb5_error_code kerror;
+  VALUE v_bool = Qtrue;
 
   Data_Get_Struct(self, RUBY_KRB5_CCACHE, ptr);
 
@@ -137,8 +169,21 @@ static VALUE rkrb5_ccache_destroy(VALUE self){
 
   kerror = krb5_cc_destroy(ptr->ctx, ptr->ccache);
 
-  if(kerror)
-    rb_raise(cKrb5Exception, "krb5_cc_destroy: %s", error_message(kerror));
+  // Don't raise an error if there's no cache. Just return false.
+  if(kerror){
+    if((kerror == KRB5_CC_NOTFOUND) || (kerror == KRB5_FCC_NOFILE)){
+      v_bool = Qfalse;
+    }
+    else{
+      if(ptr->principal)
+        krb5_free_principal(ptr->ctx, ptr->principal);
+
+      if(ptr->ctx)
+        krb5_free_context(ptr->ctx);
+
+      rb_raise(cKrb5Exception, "krb5_cc_destroy: %s", error_message(kerror));
+    }
+  }
 
   if(ptr->principal)
     krb5_free_principal(ptr->ctx, ptr->principal);
@@ -150,7 +195,7 @@ static VALUE rkrb5_ccache_destroy(VALUE self){
   ptr->ctx = NULL;
   ptr->principal = NULL;
 
-  return self;
+  return v_bool;
 }
 
 void Init_ccache(){
@@ -161,7 +206,7 @@ void Init_ccache(){
   rb_define_alloc_func(cKrb5CCache, rkrb5_ccache_allocate);
 
   // Constructor
-  rb_define_method(cKrb5CCache, "initialize", rkrb5_ccache_initialize, 1);
+  rb_define_method(cKrb5CCache, "initialize", rkrb5_ccache_initialize, -1);
 
   // Instance Methods
   rb_define_method(cKrb5CCache, "close", rkrb5_ccache_close, 0);
