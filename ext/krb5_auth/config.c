ef24ebe21a942eaa4331bf95b1dede54de0405b8
Daniel Berger <dberger@globe.gov>
2010-08-20 13:50:24 -0600

    Added more config attributes.

--- ext/krb5_auth/config.c
+++ ext/krb5_auth/config.c
@@ -65,19 +65,63 @@ static VALUE rkadm5_config_initialize(VALUE self){
   if(ptr->config.stash_file)
     rb_iv_set(self, "@stash_file", rb_str_new2(ptr->config.stash_file));
 
+  if(ptr->config.mkey_name)
+    rb_iv_set(self, "@mkey_name", rb_str_new2(ptr->config.mkey_name));
+
+  if(ptr->config.mkey_from_kbd)
+    rb_iv_set(self, "@mkey_from_kbd", INT2FIX(ptr->config.mkey_from_kbd));
+
+  if(ptr->config.mkey_from_kbd)
+    rb_iv_set(self, "@mkey_from_kbd", INT2FIX(ptr->config.mkey_from_kbd));
+
+  if(ptr->config.enctype)
+    rb_iv_set(self, "@enctype", INT2FIX(ptr->config.enctype));
+
+  if(ptr->config.max_life)
+    rb_iv_set(self, "@enctype", INT2FIX(ptr->config.max_life));
+
+  if(ptr->config.max_rlife)
+    rb_iv_set(self, "@enctype", INT2FIX(ptr->config.max_rlife));
+
+  if(ptr->config.expiration)
+    rb_iv_set(self, "@expiration", rb_time_new(ptr->config.expiration, 0));
+
+  if(ptr->config.flags)
+    rb_iv_set(self, "@flags", INT2FIX(ptr->config.flags));
+
+  if(ptr->config.kvno)
+    rb_iv_set(self, "@kvno", INT2FIX(ptr->config.kvno));
+
+  if(ptr->config.iprop_enabled)
+    rb_iv_set(self, "@iprop_enabled", Qtrue);
+  else
+    rb_iv_set(self, "@iprop_enabled", Qfalse);
+
+  if(ptr->config.iprop_logfile)
+    rb_iv_set(self, "@iprop_logfile", rb_str_new2(ptr->config.iprop_logfile));
+
+  if(ptr->config.iprop_poll_time)
+    rb_iv_set(self, "@iprop_poll_time", INT2FIX(ptr->config.iprop_poll_time));
+
+  if(ptr->config.iprop_port)
+    rb_iv_set(self, "@iprop_port", INT2FIX(ptr->config.iprop_port));
+
   return self;
 }
 
 void Init_config(){
   cKadm5Config = rb_define_class_under(cKadm5, "Config", rb_cObject);
 
-  // Allocation functions
+  // Allocation function
+
   rb_define_alloc_func(cKadm5Config, rkadm5_allocate);
   
-  // Initializers
+  // Initializer
+
   rb_define_method(cKadm5Config, "initialize", rkadm5_config_initialize, 0);
 
   // Accessors
+
   rb_define_attr(cKadm5Config, "realm", 1, 0);
   rb_define_attr(cKadm5Config, "admin_server", 1, 0);
   rb_define_attr(cKadm5Config, "kadmind_port", 1, 0);
@@ -86,4 +130,16 @@ void Init_config(){
   rb_define_attr(cKadm5Config, "acl_file", 1, 0);
   rb_define_attr(cKadm5Config, "dict_file", 1, 0);
   rb_define_attr(cKadm5Config, "stash_file", 1, 0);
+  rb_define_attr(cKadm5Config, "mkey_name", 1, 0);
+  rb_define_attr(cKadm5Config, "mkey_from_kbd", 1, 0);
+  rb_define_attr(cKadm5Config, "enctype", 1, 0);
+  rb_define_attr(cKadm5Config, "max_life", 1, 0);
+  rb_define_attr(cKadm5Config, "max_rlife", 1, 0);
+  rb_define_attr(cKadm5Config, "expiration", 1, 0);
+  rb_define_attr(cKadm5Config, "flags", 1, 0);
+  rb_define_attr(cKadm5Config, "kvno", 1, 0);
+  rb_define_attr(cKadm5Config, "iprop_enabled", 1, 0);
+  rb_define_attr(cKadm5Config, "iprop_logfile", 1, 0);
+  rb_define_attr(cKadm5Config, "iprop_poll_time", 1, 0);
+  rb_define_attr(cKadm5Config, "iprop_port", 1, 0);
 }
--- test/test_config.rb
+++ test/test_config.rb
@@ -54,6 +54,61 @@ class TC_Kadm5_Config < Test::Unit::TestCase
     assert_kind_of([String, NilClass], @config.stash_file)
   end
 
+  test "mkey_name basic functionality" do
+    assert_respond_to(@config, :mkey_name)
+    assert_kind_of([String, NilClass], @config.mkey_name)
+  end
+
+  test "mkey_from_kbd basic functionality" do
+    assert_respond_to(@config, :mkey_from_kbd)
+    assert_kind_of([Fixnum, NilClass], @config.mkey_from_kbd)
+  end
+
+  test "enctype basic functionality" do
+    assert_respond_to(@config, :enctype)
+    assert_kind_of(Fixnum, @config.enctype)
+  end
+
+  test "max_life basic functionality" do
+    assert_respond_to(@config, :max_life)
+    assert_kind_of([Fixnum, NilClass], @config.max_life)
+  end
+
+  test "max_rlife basic functionality" do
+    assert_respond_to(@config, :max_rlife)
+    assert_kind_of([Fixnum, NilClass], @config.max_rlife)
+  end
+
+  test "expiration basic functionality" do
+    assert_respond_to(@config, :expiration)
+    assert_kind_of([Time, NilClass], @config.expiration)
+  end
+
+  test "kvno basic functionality" do
+    assert_respond_to(@config, :kvno)
+    assert_kind_of([Fixnum, NilClass], @config.kvno)
+  end
+
+  test "iprop_enabled basic functionality" do
+    assert_respond_to(@config, :iprop_enabled)
+    assert_boolean(@config.iprop_enabled)
+  end
+
+  test "iprop_logfile basic functionality" do
+    assert_respond_to(@config, :iprop_logfile)
+    assert_kind_of(String, @config.iprop_logfile)
+  end
+
+  test "iprop_polltime basic functionality" do
+    assert_respond_to(@config, :iprop_poll_time)
+    assert_kind_of(Fixnum, @config.iprop_poll_time)
+  end
+
+  test "iprop_port basic functionality" do
+    assert_respond_to(@config, :iprop_port)
+    assert_kind_of([Fixnum, NilClass], @config.iprop_port)
+  end
+
   def teardown
     @config = nil
   end
