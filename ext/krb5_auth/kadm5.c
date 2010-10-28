94e80b82e2db781ffde8326d9a37859a8bac6d0c
Daniel Berger <dberger@globe.gov>
2010-10-28 10:13:03 -0600

    Added the kadm5.find_principal method and corresponding tests.

--- ext/krb5_auth/kadm5.c
+++ ext/krb5_auth/kadm5.c
@@ -312,6 +312,103 @@ static VALUE rkadm5_close(VALUE self){
 
 /*
  * call-seq:
+ *   kadm5.find_principal(principal_name)
+ *
+ * Returns a Principal object for +principal_name+ containing various bits
+ * of information regarding that principal, such as policy, attributes,
+ * expiration information, etc.
+ *
+ * Unlike the get_principal method, this method returns nil if the principal
+ * cannot be found instead of raising an error.
+ */
+static VALUE rkadm5_find_principal(VALUE self, VALUE v_user){
+  RUBY_KADM5* ptr;
+  VALUE v_principal;
+  VALUE v_args[1];
+  char* user;
+  int mask;
+  kadm5_principal_ent_rec ent;
+  krb5_error_code kerror;
+
+  Data_Get_Struct(self, RUBY_KADM5, ptr);
+  Check_Type(v_user, T_STRING);
+  user = StringValuePtr(v_user);
+
+  memset(&ent, 0, sizeof(ent));
+
+  if(!ptr->ctx)
+    rb_raise(cKadm5Exception, "no context has been established");
+
+  kerror = krb5_parse_name(ptr->ctx, user, &ptr->princ);
+
+  if(kerror)
+    rb_raise(cKadm5Exception, "krb5_parse_name: %s", error_message(kerror));
+
+  mask = KADM5_PRINCIPAL_NORMAL_MASK;
+
+  kerror = kadm5_get_principal(
+    ptr->handle,
+    ptr->princ,
+    &ent,
+    mask
+  );
+
+  // Return nil if not found instead of raising an error.
+  if(kerror){
+    if(kerror == KADM5_UNK_PRINC)
+      return Qnil;
+
+    rb_raise(cKadm5Exception, "kadm5_get_principal: %s", error_message(kerror));
+  }
+
+  v_args[0] = v_user;
+  v_principal = rb_class_new_instance(1, v_args, cKrb5Principal);
+
+  rb_iv_set(v_principal, "@attributes", LONG2FIX(ent.attributes));
+  rb_iv_set(v_principal, "@aux_attributes", INT2FIX(ent.aux_attributes));
+
+  if(ent.princ_expire_time)
+    rb_iv_set(v_principal, "@expire_time", rb_time_new(ent.princ_expire_time, 0));
+
+  rb_iv_set(v_principal, "@fail_auth_count", INT2FIX(ent.fail_auth_count));
+  rb_iv_set(v_principal, "@kvno", INT2FIX(ent.kvno));
+
+  if(ent.last_failed)
+    rb_iv_set(v_principal, "@last_failed", rb_time_new(ent.last_failed, 0));
+
+  if(ent.last_failed)
+    rb_iv_set(v_principal, "@last_password_change", rb_time_new(ent.last_pwd_change, 0));
+
+  if(ent.last_failed)
+    rb_iv_set(v_principal, "@last_success", rb_time_new(ent.last_success, 0));
+
+  rb_iv_set(v_principal, "@max_life", LONG2FIX(ent.max_life));
+  rb_iv_set(v_principal, "@max_renewable_life", LONG2FIX(ent.max_renewable_life));
+
+  if(ent.mod_date)
+    rb_iv_set(v_principal, "@mod_date", rb_time_new(ent.mod_date, 0));
+
+  if(ent.mod_name){
+    char* mod_name;
+    kerror = krb5_unparse_name(ptr->ctx, ent.mod_name, &mod_name);
+
+    if(kerror)
+      rb_raise(cKadm5Exception, "krb5_unparse_name: %s", error_message(kerror));
+
+    rb_iv_set(v_principal, "@mod_name", rb_str_new2(mod_name));
+  }
+
+  if(ent.pw_expiration)
+    rb_iv_set(v_principal, "@password_expiration", rb_time_new(ent.pw_expiration, 0));
+
+  if(ent.policy)
+    rb_iv_set(v_principal, "policy", rb_str_new2(ent.policy));
+
+  return v_principal;
+}
+
+/*
+ * call-seq:
  *   kadm5.get_principal(principal_name)
  *
  * Returns a Principal object for +principal_name+ containing various bits
@@ -455,6 +552,7 @@ void Init_kadm5(){
   rb_define_method(cKadm5, "create_policy", rkadm5_create_policy, 1);
   rb_define_method(cKadm5, "create_principal", rkadm5_create_principal, 2);
   rb_define_method(cKadm5, "delete_principal", rkadm5_delete_principal, 1);
+  rb_define_method(cKadm5, "find_principal", rkadm5_find_principal, 1);
   rb_define_method(cKadm5, "get_principal", rkadm5_get_principal, 1);
   rb_define_method(cKadm5, "set_password", rkadm5_set_password, 2);
 
